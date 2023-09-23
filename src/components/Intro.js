a133a7f7ee1a028cda6c82ccb49bb0999367c99c
willcoderhz <125367182+willcoderhz@users.noreply.github.com>
2023-09-23 13:56:12 +0800

    font

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Intro() {
     return (
         <div className='intros'>
-            <h1>水浒叶子</h1>
+            <h1>水滸葉子</h1>
            
         </div>
     );
